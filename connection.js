482d65a8dca18ccdea1adf0fd4fde574d6d1d610
Anjana-Ashok <anjanaashok987@gmail.com>
2024-09-30 11:52:52 +0530

    blog

---
+++ connection.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+//Write missing code here for database connection
+const mongoURI = "mongodb://localhost:27017/blogDB";
+
+mongoose.connect(mongoURI).then(()=>{
+    console.log('Connection established');
+}).catch(()=>{
+    console.log('Error in connection');
+})
